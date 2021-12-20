811ab4216d442aa21ccaa03dbd6e17ed1fb3f048
Andrew Borys <borysandrey1988@gmail.com>
2021-12-20 13:54:37 +0200

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import galleryItemTpl from '../templates/gallery-items.hbs';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMurkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMurkup);
+galleryRef.addEventListener('click', onImageToCreateModal);
+
+function createGalleryMarkup(gallery) {
+  return gallery.map(galleryItemTpl).join('');
+}
+
+function onImageToCreateModal(e) {
+  e.preventDefault();
+  const gallery = new SimpleLightbox('.gallery a');
+}
