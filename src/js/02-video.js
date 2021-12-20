ef0ab74334e3ac570cf14ecff9cabcc98aaad9ef
Andrew Borys <borysandrey1988@gmail.com>
2021-12-20 22:44:39 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+      // seconds = the current playback position
+      // console.log(seconds);
+
+      localStorage.setItem('playerCurrentPosition', JSON.stringify(seconds));
+    });
+  }, 1000),
+);
+
+const savedTime = localStorage.getItem('playerCurrentPosition');
+const playerStartTime = JSON.parse(savedTime);
+
+// console.log(playerStartTime);
+
+player.setCurrentTime(playerStartTime);
