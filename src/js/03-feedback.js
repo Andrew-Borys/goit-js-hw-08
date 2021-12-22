ebbb6efb338e28f0cfe05c1304108f71f62db746
Andrew Borys <borysandrey1988@gmail.com>
2021-12-22 02:41:44 +0200

    corrected 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,8 @@ const refs = {
   submitBtn: document.querySelector('.feedback-form button'),
 };
 
-refs.submitBtn.addEventListener('click', onFormSubmit);
+refs.submitBtn.addEventListener('click', onFormSubmit, onFormInput);
+// refs.form.addEventListener('input', onFormInput);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 const STORAGE_KEY = 'feedback-form-state';
@@ -19,27 +20,27 @@ function onFormSubmit(e) {
 
   refs.form.reset();
 
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormInput(e) {
-  // const formElements = e.currentTarget.elements;
+  const mail = refs.email.value;
+  const message = refs.message.value;
 
-  const mail = formElements.email.value;
-  const message = formElements.message.value;
-
-  const dataToSave = {
+  const dataStorage = {
     mail,
     message,
   };
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataToSave));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
 }
 
 function onFormShow() {
   const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (refs.email || refs.message === '') {
+  if (formData) {
     refs.email.value = formData.mail;
     refs.message.value = formData.message;
   }
